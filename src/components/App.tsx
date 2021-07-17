b5df4593a03f671cb67e100946715cf6e4335ef6
joewickes <joe.c.wickes@gmail.com>
2021-07-16 21:09:54 -0400

    Connect route to switch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,13 +5,16 @@ import { Switch, Route } from 'react-router-dom';
 // Style(s)
 import './../styles/App.css';
 
+// Route(s)
+import HomePage from '../routes/HomePage';
+
 // Component(s)
 
 const App : FC<{/* Props */}> = ({/* Destructured props */}) => {
   return (
     <div className='App'>
       <Switch>
-        <Route exact path='/' /> {/*component={ () => <Component /> }*/} 
+        <Route exact path='/' /> component={() => <HomePage /> }
       </Switch>
     </div>
   );
